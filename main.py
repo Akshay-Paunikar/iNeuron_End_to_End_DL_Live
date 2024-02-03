8eb77344563f15359ef07ce08e78442665f9176c
Akshay Paunikar <akshayakki29@gmail.com>
2024-02-04 00:45:52 +0530

    Data Ingestion Notebook Experiment and Final Implementation Successfull

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
 import sys
+
 from src.cnnClassifier.logger import logging
 from src.cnnClassifier.exception import CustomException
 
-def divide(x:int, y:int) -> int:
-    result = x / y
-    return result
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+STAGE_NAME = "Data Ingestion stage"
 try:
-    divide(5,0)
-    logging.info("divide operation successful")
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logging.info("Error occured please check the error message")
-    raise CustomException(e,sys)
+        logging.exception(e)
+        raise CustomException(e,sys)
