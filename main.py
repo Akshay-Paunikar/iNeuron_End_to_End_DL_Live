f1df418ab17671d7ddbcb28b0c6647b8fdce9e98
Akshay Paunikar <akshayakki29@gmail.com>
2024-02-04 23:02:12 +0530

    Prepare Base Model Notebook Experiment and Final Implementation successfull

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ from src.cnnClassifier.logger import logging
 from src.cnnClassifier.exception import CustomException
 
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
-STAGE_NAME = "Data Ingestion stage"
-try:
-   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
+# STAGE_NAME = "Data Ingestion stage"
+# try:
+#    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_ingestion = DataIngestionTrainingPipeline()
+#    data_ingestion.main()
+#    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logging.exception(e)
+#         raise CustomException(e,sys)
+     
+STAGE_NAME = "Prepare base model"
+try: 
+   logging.info(f"*******************")
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logging.exception(e)
