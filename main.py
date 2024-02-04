d17df111116c16b6257e2f85485e62c12f21c10f
Akshay Paunikar <akshayakki29@gmail.com>
2024-02-05 01:09:35 +0530

    Training Notebook Experiment and Final Implementation Successfull

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from src.cnnClassifier.exception import CustomException
 
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
-# STAGE_NAME = "Data Ingestion stage"
-# try:
-#    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataIngestionTrainingPipeline()
-#    data_ingestion.main()
-#    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logging.exception(e)
-#         raise CustomException(e,sys)
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise CustomException(e,sys)
      
 STAGE_NAME = "Prepare base model"
 try: 
@@ -23,6 +24,17 @@ try:
    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise CustomException(e,sys)
+
+STAGE_NAME = "Training"
+try: 
+   logging.info(f"*******************")
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logging.exception(e)
