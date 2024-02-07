10eb70539643030a168deb3176bbc056937eb333
Akshay Paunikar <akshayakki29@gmail.com>
2024-02-08 00:15:35 +0530

    Testing app on render failed will try later again

--- app.py
+++ app.py
@@ -1,57 +1,33 @@
 from flask import Flask, request, jsonify, render_template
 import os
+import sys
 from flask_cors import CORS, cross_origin
 from src.cnnClassifier.utils import decodeImage
-from predict import PredictionPipeline
+from src.cnnClassifier.pipeline.predict import PredictionPipeline
 from src.cnnClassifier.logger import logging
 from src.cnnClassifier.exception import CustomException
-from ensure import ensure_annotations
-import base64
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
-@ensure_annotations
-def decodeImage(imgstring, fileName):
-    """
-    to decode the binary string into normal form.
-    
-    Returns:
-        Return the decoded string.    
-    """
-    try:
-        imgdata = base64.b64decode(imgstring)
-        with open(fileName, "wb") as f:
-            f.write(imgdata)
-            f.close()
-            logging.info("image data decoded successfully using decodeImage function")
-    except Exception as e:
-        logging.info("Error in performing operation decodeImage")
-        raise CustomException(e,sys)
-
 app = Flask(__name__)
 CORS(app)
-
 
 class ClientApp:
     def __init__(self):
         self.filename = "inputImage.jpg"
         self.classifier = PredictionPipeline(self.filename)
 
-
 @app.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
-
 
 @app.route("/train", methods=['GET','POST'])
 @cross_origin()
 def trainRoute():
     os.system("python main.py")
     return "Training done successfully!"
-
-
 
 @app.route("/predict", methods=['POST'])
 @cross_origin()
@@ -60,7 +36,6 @@ def predictRoute():
     decodeImage(image, clApp.filename)
     result = clApp.classifier.predict()
     return jsonify(result)
-
 
 if __name__ == "__main__":
     clApp = ClientApp()
